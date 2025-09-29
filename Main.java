ad16cbc13e1b3c09a6c22d8bd0a08cbc7f755c99
huttatran <hdtran114@student.hbuhsd.edu>
2025-09-29 21:44:05 +0000

    commited

--- Main.java
+++ Main.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
+
 public class Main
 {
   public static void main(String[] args)
   {
-    // write your code here
+    Scanner scan = new Scanner(System.in);
+    System.out.println("give two words");
+    String word1 = scan.nextLine();
+    String word2 = scan.nextLine();
+    System.out.println("Give me a number");
+    int N = scan.nextInt();
+    scan.nextLine();
+    lastFirstN(word1, word2, N);
+
+  //  lastFirstN("cream", "butter", 3);
+  //  stringManip("hello", "hello");
+
+    System.out.println("give me two words again");
+    String word1P2 = scan.nextLine();
+    String word2P2 = scan.nextLine();
+    stringManip(word1P2, word2P2);
   }
 
   /** Precondition: s1 and s2 are not null
@@ -10,14 +27,22 @@ public class Main
   */
   public static void lastFirstN(String s1, String s2, int n)
   {
-  
+    String lastNofS1 = s1.substring(s1.length() - n);
+    String firstNofS2 = s2.substring(0, n);
+    System.out.println(lastNofS1 + firstNofS2);
   }
 
   /** Precondition: s1 and s2 are not null
   */
   public static void stringManip(String s1, String s2)
   {
-  
+    
+    String s1Upper = s1.toUpperCase();
+    String s2Temp = s2.substring(1);
+    String s2First = s2.substring(0,1);
+    String s2Final = s2First.toUpperCase() + s2Temp;
+    System.out.println(s1Upper);
+    System.out.println(s2Final);
   }
 
   /** Precondition: s1 and s2 are not null
